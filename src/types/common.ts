2da367134f9db3fdf43d48f8dc662e6a2bc3a270
Drakonkinst <drakonkinstmc@gmail.com>
2026-03-28 22:07:49 -0700

    Delete unused code

--- src/types/common.ts
+++ src/types/common.ts
@@ -56,50 +56,15 @@ export const KeywordsSchema = z.array(KeywordSchema).nonempty().meta({
     "id": "Keywords"
 });
 
-export const CategoriesSchema = z.union([z.string().nonempty(), z.array(z.string().nonempty()).nonempty()]).meta({
-    "id": "Categories"
-});
-
-export const DirectionSchema = z.enum(["Forward", "Backward", "Any"]).meta({
-    "id": "Direction"
-});
-export const StrikeSchema = z.enum(["Thrust", "Cut", "Oberhau", "Unterhau", "Mittlehau"]).meta({
-    "id": "Strike"
-});
 export const ActionTypeSchema = z.enum(["Arm", "Leg", "Special"]).meta({
     "id": "ActionType"
 });
 export const ActionTypesSchema = z.union([ActionTypeSchema, z.array(ActionTypeSchema).nonempty()]);
-export const TargetSchema = z.enum(["High", "Low"]).meta({
-    "id": "Target"
-});
 export const ParryHeightSchema = z.enum(["High", "Low", "Both", "None"]).meta({
     "id": "ParryHeight"
 });
-export const GuardArchetypeSchema = z.enum(["Ochs", "Pflug", "VomTag", "Alber"]).meta({
-    "id": "GuardArchetype",
-    "title": "Guard Archetype",
-    "description": "Which of the Vier Leger (core guards) this position would be classified as."
-});
-export const PlayerSchema = z.enum(["Self", "Opponent"]).meta({
-    "id": "Player",
-    "title": "Player",
-    "description": "Self or Opponent"
-});
-export const StatSchema = z.enum(["Speed", "Structure"]).meta({
-    "id": "Stat",
-    "title": "Stat",
-    "description": "Core stat of the card. Speed or Structure"
-});
-export const CardIdSchema = z.string().nonempty().describe("The ID of the card.");
-export const GuardIdSchema = z.string().nonempty().describe("The ID of the guard.");
-export const ArmActionTypeSchema = z.enum(["Normal", "Parry", "Defend", "Chamber"]).meta({
-    "id": "ArmActionType"
-});
-export const MetaTypeSchema = z.enum(["None", "Token"]);
 
 export type Keyword = z.infer<typeof KeywordSchema>;
 export type ValueRange = z.infer<typeof ValueRangeSchema>;
 export type CardText = z.infer<typeof CardTextSchema>;
 export type ActionType = z.infer<typeof ActionTypeSchema>;
-export type MetaType = z.infer<typeof MetaTypeSchema>;
